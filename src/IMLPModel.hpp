b5cf62058cd378542960e824472e4127abe98a48
Lion Sherry <lsherry@va-j1.kzn.21-school.ru>
2023-01-31 20:10:26 +0300

    not working perceptrone
    Former-commit-id: f6640da7cd040fd92f88741a831f41a8c61642a3 [formerly 970782d3a1eb38d2e9162b7599e48431fbfc45ab]
    Former-commit-id: e9912d399c56ac940d993d2f5080a415e2f94053

--- src/IMLPModel.hpp
+++ src/IMLPModel.hpp
@@ -1,18 +1,24 @@
+#ifndef IMLPMODEL_HPP
+#define IMLPMODEL_HPP
+
+
+
 #include <vector>
+#include "Matrix.hpp"
 
 namespace s21 {
 	template <typename T>
 	class IMLPModel {
-	protected:
-		virtual int Forward(std::vector<T>) = 0;
-		virtual int Backward(std::vector<T>) = 0;
-		IMLPModel();
 	public:
+		virtual std::vector<T> Forward(Matrix<T>) = 0;
+		virtual void Backward(Matrix<T>) = 0;
 		virtual int Import() = 0;
 		virtual int Export() = 0;
-		virtual int Train(std::vector<T>) = 0;
-		virtual int Test(std::vector<T>) = 0;
+		virtual int Train() = 0;
+		virtual int Test() = 0;
 		virtual int Predict() = 0;
-		virtual IMLPModel *MakeModel(size_t in_channels, size_t out_channels, size_t hidden_units_per_layer, int hidden_layers, float lr);
+//		virtual IMLPModel *Instance(size_t in_channels, size_t out_channels, size_t hidden_units_per_layer, int hidden_layers, float lr);
 	};
 }
+
+#endif
