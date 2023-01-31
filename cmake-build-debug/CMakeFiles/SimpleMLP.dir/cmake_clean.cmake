f5d2a0e88d6917b9e7a777a7b1dc58fe3a02478c
Lion Sherry <lsherry@va-j1.kzn.21-school.ru>
2023-01-31 15:12:24 +0300

    added << operator, removed print
    Former-commit-id: 4dc5c9b050ca6586f0616c697cf08eb7c0acee05

---
+++ cmake-build-debug/CMakeFiles/SimpleMLP.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimpleMLP.dir/src/Matrix.cpp.o"
+  "CMakeFiles/SimpleMLP.dir/src/Matrix.cpp.o.d"
+  "CMakeFiles/SimpleMLP.dir/src/main.cpp.o"
+  "CMakeFiles/SimpleMLP.dir/src/main.cpp.o.d"
+  "SimpleMLP"
+  "SimpleMLP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SimpleMLP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
