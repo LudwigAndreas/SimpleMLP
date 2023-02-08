21b76f51d99cdd99778e6a79e8da1d32d591b639
Andrey Kalinin <kalininandrey727@gmail.com>
2023-02-08 15:41:43 +0300

    changed serializer to save all data from model
    Former-commit-id: f81929fa42a75045da1ad0fdc96e78d95b5327d5 [formerly dfb91923168eed4b1d856bbe2a7268cf124bb31b]
    Former-commit-id: fc5309cba232c290e9952d74b9638c5151627cc2
    Former-commit-id: 3575d49aaefb3f9b7da7aa9eda65b627e9b0b6e2
    Former-commit-id: 43319fa25cd8d366f19f77dfdbd802f959b21c82

--- src/main.cpp
+++ src/main.cpp
@@ -105,15 +105,15 @@ std::vector<s21::Sample> ReadDataset(std::string filename)
 }
 
 int main() {
-	// s21::IMLPModel<float>		*model = s21::MLPMatrixModelv2::MakeModel(784, 26, 256, 2, .1f);
+	s21::IMLPModel<float>		*model = s21::MLPMatrixModelv2::MakeModel(784, 26, 256, 2, .1f);
 	std::vector<s21::Sample>	samples;
 	// std::chrono::time_point<std::chrono::system_clock> start, end;
-	auto model = s21::MLPMatrixModelv2::MakeModel(0, 0, 0, 0, 0);
-	MLPSerializer<float>::DeserializeMLPMatrixModel((s21::MLPMatrixModelv2 *)model, "testmodel.mlpmodel");
+	// auto model = s21::MLPMatrixModelv2::MakeModel(0, 0, 0, 0, 0);
+	// MLPSerializer<float>::DeserializeMLPMatrixModel((s21::MLPMatrixModelv2 *)model, "testmodel.mlpmodel");
 
 
-	s21::Dataset dataset(ReadDataset("datasets/emnist-letters-test.csv"), 16);
-	// s21::Dataset dataset(ReadDataset("datasets/emnist-letters-train.csv"), 16);
+	// s21::Dataset dataset(ReadDataset("datasets/emnist-letters-test.csv"), 16);
+	s21::Dataset dataset(ReadDataset("datasets/emnist-letters-train.csv"), 32);
 	// s21::Dataset dataset(samples, 32);
 	// std::cerr << "Dataset split on " << dataset.size() << " with " << dataset[0].size() << " samples in each." << std::endl;
 	CrossValidation(model, dataset);
