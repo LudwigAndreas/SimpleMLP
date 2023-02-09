113706f79dae739512c05418c55a4e7324f051ef
LudwigAndreas <landreas@MacBook-Pro-3.Dlink>
2023-02-09 15:44:07 +0300

    added gui folder, moved mlp to core/ folder, changed cmakelist.txt to build project with qt gui
    Former-commit-id: 98f80205afe74b38d94be09c216cd065c203fe9b [formerly e97d7336bdb336be09555bd229a87c9a0c8f65c8]
    Former-commit-id: 24aad7b1ba968acbc8760540464250cfade29141

---
+++ src/gui/mainwindow.h
@@ -0,0 +1,66 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QFile>
+#include <QGraphicsScene>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+
+    void on_start_model_button_clicked();
+
+    void on_train_model_push_button_pressed();
+
+    void on_test_model_push_button_pressed();
+
+    void on_tabWidget_currentChanged(int index);
+
+    void on_predict_push_button_pressed();
+
+    void on_start_training_push_button_pressed();
+
+    void on_toolButton_pressed();
+
+    void on_model_config_was_uploaded();
+
+    void on_training_dataset_was_uploaded();
+
+    void on_testing_dataset_was_uploaded();
+
+    void on_toolButton_3_pressed();
+
+    void on_test_model_push_button_2_pressed();
+
+    void on_pushButton_pressed();
+
+    void on_toolButton_4_pressed();
+
+    void on_back_to_configure_push_button_pressed();
+
+    void on_back_to_configure_push_button_2_pressed();
+
+    void on_testing_size_horizontalSlider_valueChanged(int value);
+
+private:
+    Ui::MainWindow *ui;
+    QFile *model_config_file;
+    QFile *training_dataset_file;
+    QFile *testing_dataset_file;
+
+    void modelConfigFileWasUploaded(QFile *file);
+    void trainDatasetFileWasUploaded(QFile *file);
+    void testDatasetFileWasUploaded(QFile *file);
+};
+#endif // MAINWINDOW_H
