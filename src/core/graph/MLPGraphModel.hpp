17b3be763ff55d4b5730394be4e097fc5f3134c6
Andrey Kalinin <kalininandrey727@gmail.com>
2023-03-11 18:11:10 +0300

    interfaces half-done for graph classes

--- src/core/graph/MLPGraphModel.hpp
+++ src/core/graph/MLPGraphModel.hpp
@@ -1,10 +1,15 @@
 #pragma once
 
 #include "../utils/IMLPModel.hpp"
+#include "../graph/MLPGraphLayer.hpp"
 
 namespace s21 {
 	class MLPGraphModel : public IMLPModel<float> {
-
-	}
+	private:
+		std::vector<MLPGraphLayer *> layers;
+	
+	public:
+		MLPGraphModel *AddLayer(MLPGraphLayer *layer);
+	};
 
 }
