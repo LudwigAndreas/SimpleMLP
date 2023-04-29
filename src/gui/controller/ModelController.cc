3be15317e4de164dae23ffda91910bc9a68fe6e5
Ludwig Andreas <landreas-pc@MacPro.local>
2023-04-29 16:24:25 +0300

    added Controller
    Former-commit-id: b494a2aac410948e2dc4ef16359041f311d99a6c [formerly 590f024ec6d8226ff0eabfdfd8eb9a0f8a2410c7]
    Former-commit-id: 4f072f67c92f233cd0176dedc6f082f0453c12ae

---
+++ src/gui/controller/ModelController.cc
@@ -0,0 +1,60 @@
+#include "ModelController.h"
+
+ModelController::ModelController(QMainWindow* parent) {
+
+}
+
+ModelController::~ModelController() {
+
+}
+
+void ModelController::ConfigureModel() {
+
+}
+
+void ModelController::TrainModel() {
+
+}
+
+void ModelController::TestModel() {
+
+}
+
+void ModelController::HandleGoToConfigurePageButtonClicked() {
+}
+
+void ModelController::HandleGoToTrainingPageButtonClicked() {
+
+}
+
+void ModelController::HandleGoToTestingPageButtonClicked() {
+
+}
+
+void ModelController::HandleImportModel(QFile* file) {
+
+}
+
+void ModelController::HandleImportTrainingDataset(QFile* file) {
+
+}
+
+void ModelController::HandleImportTestingDataset(QFile* file) {
+
+}
+
+void ModelController::HandleStartTraining() {
+
+}
+
+void ModelController::HandleExportModel() {
+
+}
+
+void ModelController::HandleModelPrediction() {
+
+}
+
+void ModelController::HandleFileWasDrawn() {
+
+}
--- src/gui/view/mainwindow.cpp
+++ src/gui/view/mainwindow.cpp
@@ -39,6 +39,7 @@ MainWindow::MainWindow(QWidget *parent)
 	ui->chart_widget->setRenderHint(QPainter::Antialiasing);
 //		ui->chart_widget->setCurrentWidget(chart);
 
+<<<<<<< Updated upstream
 	ui->testing_progress_bar->setValue(0);
 	ui->testing_progress_bar->hide();
     ui->training_progress_bar->setValue(0);
@@ -52,6 +53,33 @@ MainWindow::MainWindow(QWidget *parent)
 			this, &MainWindow::onTestingDatasetWasUploaded);
     connect(ui->paint_view, &PaintView::file_saved,
 			this, &MainWindow::onFileWasDrawn);
+=======
+  this->m_controller = new ModelController(this);
+
+  ui->stackedWidget->setCurrentIndex(0);
+  ui->chart_widget->hide();
+
+  this->chart_series = new QLineSeries();
+  auto *chart = new QChart();
+  chart->legend()->hide();
+  chart->addSeries(this->chart_series);
+  auto *y = new QValueAxis();
+  y->setMax(100);
+  y->setMin(0.1);
+  chart->addAxis(y, Qt::AlignLeft);
+  chart_series->attachAxis(y);
+  ui->chart_widget->setChart(chart);
+  ui->chart_widget->setRenderHint(QPainter::Antialiasing);
+
+  ui->testing_progress_bar->setValue(0);
+  ui->testing_progress_bar->hide();
+  ui->training_progress_bar->setValue(0);
+  ui->training_progress_bar->hide();
+  ui->paint_view->setBackgroundBrush(QColor("midnightblue"));
+
+  CreateConnectionWithController();
+  CreateConnectionWithView();
+>>>>>>> Stashed changes
 }
 
 MainWindow::~MainWindow()
@@ -59,4 +87,76 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+<<<<<<< Updated upstream
+=======
+void MainWindow::CreateConnectionWithView() {
+  /* Navigation */
+  connect(ui->to_configure_push_button, &QPushButton::pressed, this,
+          &MainWindow::on_to_configure_push_button_pressed);
+  connect(ui->to_configure_push_button_2, &QPushButton::pressed, this,
+          &MainWindow::on_to_configure_push_button_2_pressed);
+  connect(ui->to_train_push_button, &QPushButton::pressed, this,
+          &MainWindow::on_to_train_push_button_pressed);
+  connect(ui->train_model_push_button, &QPushButton::pressed, this,
+          &MainWindow::on_train_model_push_button_pressed);
+  connect(ui->test_model_push_button, &QPushButton::pressed, this,
+          &MainWindow::on_test_model_push_button_pressed);
+  connect(ui->test_model_push_button_2, &QPushButton::pressed, this,
+          &MainWindow::on_test_model_push_button_2_pressed);
+//  /* Drag&Drop fields */
+//  connect(ui->import_model_config_label, SIGNAL(fileWasUploaded(QFile *)),
+//          m_controller, SLOT(HandleImportModel(QFile *)));
+//  connect(ui->import_train_dataset_label, SIGNAL(fileWasUploaded(QFile *)),
+//          m_controller, SLOT(HandleImportTrainingDataset(QFile *)));
+//  connect(ui->import_test_dataset_label, SIGNAL(fileWasUploaded(QFile *)),
+//          m_controller, SLOT(HandleImportTestingDataset(QFile *)));
+//  /* Start training process */
+//  connect(ui->start_training_push_button, &QPushButton::pressed, m_controller,
+//          &ModelController::HandleStartTraining);
+//  /* Export */
+//  connect(ui->export_model_push_button, &QPushButton::pressed, m_controller,
+//          &ModelController::HandleExportModel);
+//  /* Prediction */
+//  connect(ui->predict_push_button, &QPushButton::pressed, m_controller,
+//          &ModelController::HandleModelPrediction);
+//  /* Drawing */
+//  connect(ui->paint_view, &PaintView::file_saved,
+//          m_controller, &ModelController::HandleFileWasDrawn);
+}
+
+void MainWindow::CreateConnectionWithController() {
+  /* Navigation */
+  connect(ui->to_configure_push_button, &QPushButton::pressed, m_controller,
+          &ModelController::HandleGoToConfigurePageButtonClicked);
+  connect(ui->to_configure_push_button_2, &QPushButton::pressed, m_controller,
+          &ModelController::HandleGoToConfigurePageButtonClicked);
+  connect(ui->to_train_push_button, &QPushButton::pressed, m_controller,
+          &ModelController::HandleGoToTrainingPageButtonClicked);
+  connect(ui->train_model_push_button, &QPushButton::pressed, m_controller,
+          &ModelController::HandleGoToTrainingPageButtonClicked);
+  connect(ui->test_model_push_button, &QPushButton::pressed, m_controller,
+          &ModelController::HandleGoToTestingPageButtonClicked);
+  connect(ui->test_model_push_button_2, &QPushButton::pressed, m_controller,
+          &ModelController::HandleGoToTestingPageButtonClicked);
+  /* Drag&Drop fields */
+  connect(ui->import_model_config_label, SIGNAL(fileWasUploaded(QFile *)),
+          m_controller, SLOT(HandleImportModel(QFile *)));
+  connect(ui->import_train_dataset_label, SIGNAL(fileWasUploaded(QFile *)),
+          m_controller, SLOT(HandleImportTrainingDataset(QFile *)));
+  connect(ui->import_test_dataset_label, SIGNAL(fileWasUploaded(QFile *)),
+          m_controller, SLOT(HandleImportTestingDataset(QFile *)));
+  /* Start training process */
+  connect(ui->start_training_push_button, &QPushButton::pressed, m_controller,
+          &ModelController::HandleStartTraining);
+  /* Export */
+  connect(ui->export_model_push_button, &QPushButton::pressed, m_controller,
+          &ModelController::HandleExportModel);
+  /* Prediction */
+  connect(ui->predict_push_button, &QPushButton::pressed, m_controller,
+          &ModelController::HandleModelPrediction);
+  /* Drawing */
+  connect(ui->paint_view, &PaintView::file_saved,
+        m_controller, &ModelController::HandleFileWasDrawn);
+}
+>>>>>>> Stashed changes
 
