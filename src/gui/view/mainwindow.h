ad2af447ea9f9d0283a865c9f04120ceaad66150
Ludwig Andreas <landreas-pc@MacPro.local>
2023-04-29 16:19:17 +0300

    extracted business logic from mainview and moved it into ModelController
    Former-commit-id: 497ec13c75e4a9d7feba640e10b2e4a1881f50dd [formerly c38101256268b7dd11318334882e784921642363]
    Former-commit-id: 4cdeb0595b4be6d3a830752814c3b5536fbd8287

--- src/gui/view/mainwindow.h
+++ src/gui/view/mainwindow.h
@@ -1,97 +1,131 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
 #include <QFile>
-#include <QGraphicsScene>
 #include <QFileDialog>
+#include <QGraphicsScene>
+#include <QLineSeries>
+#include <QMainWindow>
 #include <QMessageBox>
 #include <QThread>
-#include <QLineSeries>
 
 #include <sstream>
 
 #include "src/core/LetterRecognitionMlpModelBuilder.hpp"
-
 #include "src/core/utils/IMLPModel.hpp"
-#include "src/gui/utils/ModelTrainWorker.hpp"
+
+#include "src/gui/utils/ModelController.h"
+
 #include "src/gui/utils/ModelTestWorker.hpp"
+#include "src/gui/utils/ModelTrainWorker.hpp"
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui {
+class MainWindow;
+}
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+  Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+  MainWindow(QWidget *parent = nullptr);
+  ~MainWindow();
+
+  int getNumOfHiddenLayers() const;
+
+  std::string getActivationFunc() const;
+
+  int getHiddenUnitsPerLayer() const;
+
+  std::string getPerceptionBase() const;
+
+  float getLearningRate() const;
+
+  int getNumOfEpochs() const;
+
+  int getTestingDatasetFraction() const;
+
+  void update_testing_status(int completion);
+
+  void update_training_status(int epoch, int group, float accuracy);
+
+  void show_predict_window();
+
+  void onModelGuessed(char letter);
+
+  void ConnectController();
 
 private slots:
 
-    void on_train_model_push_button_pressed();
+  void on_train_model_push_button_pressed();
 
-    void on_test_model_push_button_pressed();
+  void on_test_model_push_button_pressed();
 
-    void on_tabWidget_currentChanged(int index);
+  void on_tabWidget_currentChanged(int index);
 
-	void on_to_train_push_button_pressed();
+  void on_to_train_push_button_pressed();
 
-    void on_predict_push_button_pressed();
+  void on_predict_push_button_pressed();
 
-    void on_start_training_push_button_pressed();
+  void on_start_training_push_button_pressed();
 
-	void update_training_status(int epoch, int group, float accuracy);
+  //  void update_training_status(int epoch, int group, float accuracy);
 
-	void on_toolButton_pressed();
+  void on_toolButton_pressed();
 
-    void OnModelConfigWasUploaded();
+  void OnModelConfigWasUploaded();
 
-    void onTrainingDatasetWasUploaded();
+  void onTrainingDatasetWasUploaded();
 
-    void onTestingDatasetWasUploaded();
+  void onTestingDatasetWasUploaded();
 
-    void on_toolButton_3_pressed();
+  void on_toolButton_3_pressed();
 
-    void on_test_model_push_button_2_pressed();
+  void on_test_model_push_button_2_pressed();
 
-    void on_pushButton_pressed();
+  void on_pushButton_pressed();
 
-    void on_toolButton_4_pressed();
+  void on_toolButton_4_pressed();
 
-    void on_to_configure_push_button_pressed();
+  void on_to_configure_push_button_pressed();
 
-    void on_to_configure_push_button_2_pressed();
+  void on_to_configure_push_button_2_pressed();
 
-    void on_testing_size_horizontal_slider_valueChanged(int value);
+  void on_testing_size_horizontal_slider_valueChanged(int value);
 
-	void onFileWasDrawn();
+  void on_export_model_push_button_pressed();
 
-	void on_export_model_push_button_pressed();
+signals:
+  void ModelConfigured();
 
-	void show_predict_window(std::vector<s21::ConfusionMatrix> *result);
+  void ModelImported(QFile *file);
 
-	void update_testing_status(int completion);
+  void TrainModel(QFile *file);
+
+  void StopTraining();
+
+  void TestModel(QFile *file);
+
+  void LetterSaved();
+
+  void SaveModel(std::string file_path);
 
 private:
-    Ui::MainWindow *ui;
-    QFile *model_config_file;
-    QFile *training_dataset_file;
-    QFile *testing_dataset_file;
-    s21::IMLPModel *current_model;
-	QThread *training_thread;
-	ModelTrainWorker *training_worker;
-	ModelTestWorker *testing_worker;
-	QThread *testing_thread;
-	s21::LetterRecognitionMLPModelBuilder *builder;
-	QLineSeries *chart_series;
+  Ui::MainWindow *ui;
+  QFile *model_config_file;
+  QFile *training_dataset_file;
+  QFile *testing_dataset_file;
+  QLineSeries *chart_series;
+  ModelController *m_controller;
 
-	bool exitFromTrainPage();
-	bool exitFromConfigPage();
-	void modelConfigFileWasUploaded(QFile *file);
-    void trainDatasetFileWasUploaded(QFile *file);
-    void testDatasetFileWasUploaded(QFile *file);
+  void ConnectDragNDrop();
+  void InitChart();
+
+  bool exitFromTrainPage();
+  bool exitFromConfigPage();
+  void modelConfigFileWasUploaded(QFile *file);
+  void trainDatasetFileWasUploaded(QFile *file);
+  void testDatasetFileWasUploaded(QFile *file);
 };
 #endif // MAINWINDOW_H
