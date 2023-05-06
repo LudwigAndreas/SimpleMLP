d63896f2c03a141af16fe4a12f451068c3d5594f
Andrey Kalinin <kalininandrey727@gmail.com>
2023-05-06 14:24:02 +0300

    started writing graph model tests

--- src/tests/core/graph/GraphModelTest.cc
+++ src/tests/core/graph/GraphModelTest.cc
@@ -1,3 +1,36 @@
-//
-// Created by Ludwig Andreas on 20.04.2023.
-//
+#include <gtest/gtest.h>
+
+#include "core/graph/MLPGraphModel.h"
+
+class MLPGraphModelTest : public ::testing::Test {
+protected:
+	void SetUp() override {
+		model = s21::MLPGraphModel(
+      {2, 3, 1},
+			s21::ActivationFunction(s21::ActivationFunction::Flags::Linear));
+
+  }
+
+	void TearDown() override {
+
+	}
+
+	s21::MLPGraphModel model;
+};
+
+TEST(MLPGraphModelTest, size) {
+  auto layers = model.get_layers();
+  ASSERT_EQ(*layers[0].size(), 2);
+  ASSERT_EQ(*layers[1].size(), 3);
+  ASSERT_EQ(*layers[2].size(), 1);
+}
+
+TEST(MLPGraphModelTest, Forward) {
+  auto layers = model.get_layers();
+  auto input = Matrix<float>({1, 0});
+  auto result = model.Forward(input);
+
+
+  ASSERT_FLOAT_EQ(result[0], 0);
+}
+
